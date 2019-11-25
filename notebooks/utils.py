eec1e0e1943dd4a9f52a6e58dac49a84ffbe1d39
silencedsre <jamakatel.shree.krishna@gmail.com>
2019-11-25 13:01:00 +0545

    updating sample filename

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import random
 
-def create_sample(filename = '../datasets/data/train.csv'):
+def create_sample(filename = '../datasets/data/train.csv', sample_filename = '../datasets/data/train_sample.csv'):
     num_lines = sum(1 for l in open(filename))
     sample = int(num_lines/10)
-    skip = sorted(random.sample(range(1,num_lines+1),num_lines-sample))
+    skip = sorted(random.sample(range(1,num_lines+1), num_lines-sample))
     print("reading csv and creating sample df")
     df = pd.read_csv(
          filename,
@@ -13,7 +13,7 @@ def create_sample(filename = '../datasets/data/train.csv'):
     )
     print("Saving sample df to csv")
     try:
-        df.to_csv('../datasets/data/testing.csv')
+        df.to_csv(sample_filename)
     except Exception as e:
-        print("Error occured while creating sample csv file")
+        print("Error occured while creating sample csv file. Error: {}".format(e))
     return "Success"
